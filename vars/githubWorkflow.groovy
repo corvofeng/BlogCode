9494e53f5a5071246c25ba27c764342cd5c4f53c
corvofeng <corvofeng@gmail.com>
2025-03-23 20:47:16 +0800

    Update the github workflow.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- vars/githubWorkflow.groovy
+++ vars/githubWorkflow.groovy
@@ -1,27 +1,17 @@
 def call(String token, String owner, String repo, String workflow, String ref, String app, String image) {
     def apiUrl = "https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflow}/dispatches"
-    
-    def payload = [
-        ref   : ref,
-        inputs: [
-            app  : app,
-            image: image
-        ]
-    ]
-
-    def connection = new URL(apiUrl).openConnection()
-    connection.setRequestMethod('POST')
-    connection.setRequestProperty('Content-Type', 'application/json')
-    connection.setRequestProperty('Authorization', "Bearer ${token}")
-    connection.doOutput = true
-
-    connection.outputStream.withWriter('UTF-8') { writer ->
-        writer << new groovy.json.JsonBuilder(payload).toString()
+    withCredentials([string(credentialsId: 'argocd-repo-action-token', variable: 'GH_TOKEN')]) {
+        sh """
+          curl --location ${apiUrl} \
+          --header 'Content-Type: application/json' \
+          --header 'Authorization: Bearer ${GH_TOKEN}' \
+          --data '{
+              "ref": "${ref}",
+              "inputs": {
+                  "app": "${app}",
+                  "image": "${image}"
+              }
+          }'
+          """
     }
-
-    def responseCode = connection.responseCode
-    def responseMessage = connection.inputStream.text
-
-    println "Response Code: ${responseCode}"
-    println "Response Message: ${responseMessage}"
 }
