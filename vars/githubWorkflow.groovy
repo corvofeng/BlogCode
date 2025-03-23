0bc6423158910743eef56436c0fddb722a3fe25f
corvofeng <corvofeng@gmail.com>
2025-03-23 20:56:42 +0800

    Update the repo.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- vars/githubWorkflow.groovy
+++ vars/githubWorkflow.groovy
@@ -1,6 +1,6 @@
-def call(String token, String owner, String repo, String workflow, String ref, String app, String image) {
+def call(String tokenID, String owner, String repo, String workflow, String ref, String app, String image) {
     def apiUrl = "https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflow}/dispatches"
-    withCredentials([string(credentialsId: 'argocd-repo-action-token', variable: 'GH_TOKEN')]) {
+    withCredentials([string(credentialsId: tokenID, variable: 'GH_TOKEN')]) {
         sh """
           curl --location ${apiUrl} \
           --header 'Content-Type: application/json' \
