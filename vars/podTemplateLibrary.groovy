a6493760552128db1289ed0d36a39b12de87e9d7
corvofeng <corvofeng@gmail.com>
2025-05-05 08:42:04 +0800

    Update podTemplateLibrary.groovy

--- vars/podTemplateLibrary.groovy
+++ vars/podTemplateLibrary.groovy
@@ -1,7 +1,7 @@
 /* groovylint-disable MethodReturnTypeRequired */
 /* groovylint-disable MethodParameterTypeRequired */
 /* groovylint-disable-next-line NoDef */
-def call(image='golang:1.24.1', body) {
+def call(image='jenkins/jnlp-agent-docker:latest', Closure body) {
     podTemplate(
         agentContainer: 'agent',
         agentInjection: true,
@@ -10,11 +10,14 @@ def call(image='golang:1.24.1', body) {
             envVar(key: 'DOCKER_TLS_VERIFY', value: ''),
         ],
         containers: [
+            // Used to connect with jenkins master
             containerTemplate(args: '9999999', command: 'sleep', image: 'jenkins/jnlp-agent-docker:latest', name: 'agent', workingDir: '/home/jenkins/agent'),
+
+            // Used for user to run their commands
             containerTemplate(name: 'runner', image: image, command: 'sleep', args: '99d')
         ]) {
         node(POD_LABEL) {
-            container('agent') {
+            container('runner') {
                 body()
             }
         }
