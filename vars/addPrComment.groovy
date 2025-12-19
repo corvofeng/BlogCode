9d7f82aed4e1fd112cfa0773afd6fe8739528c41
corvofeng <corvofeng@gmail.com>
2025-12-19 10:08:17 +0800

    Remove the cat.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- vars/addPrComment.groovy
+++ vars/addPrComment.groovy
@@ -49,7 +49,6 @@ def call(Map config = [:]) {
     echo '{
         "body": ${groovy.json.JsonOutput.toJson(message)}
     }' > ${payloadPath}
-    cat ${payloadPath}
     """
 
     withCredentials([string(credentialsId: token, variable: 'GH_TOKEN')]) {
