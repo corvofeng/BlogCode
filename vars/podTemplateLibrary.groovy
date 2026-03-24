26977bde70cb56c4119260acc20ff95733f08f5e
corvofeng <corvofeng@gmail.com>
2026-03-24 23:01:18 +0800

    Adjust the container order.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- vars/podTemplateLibrary.groovy
+++ vars/podTemplateLibrary.groovy
@@ -10,11 +10,11 @@ def call(image='jenkins/jnlp-agent-docker:latest', Closure body) {
             envVar(key: 'DOCKER_TLS_VERIFY', value: ''),
         ],
         containers: [
-            // Used to connect with jenkins master
-            containerTemplate(args: '9999999', command: 'sleep', image: 'jenkins/inbound-agent:rhel-ubi9-jdk25', name: 'agent', workingDir: '/home/jenkins/agent'),
-
             // Used for user to run their commands
             containerTemplate(name: 'runner', image: image, command: 'sleep', args: '99d')
+
+            // Used to connect with jenkins master
+            containerTemplate(args: '9999999', command: 'sleep', image: 'jenkins/inbound-agent:rhel-ubi9-jdk25', name: 'agent', workingDir: '/home/jenkins/agent'),
         ]) {
         node(POD_LABEL) {
             container('runner') {
